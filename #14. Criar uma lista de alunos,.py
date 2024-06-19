e55a53547356b8ffaf149b06ee10ff376bfc00b2
SelinZ <91801180+SelinZ1@users.noreply.github.com>
2024-06-18 23:48:02 -0300

    Add files via upload

--- #14. Criar uma lista de alunos,.py
+++ #14. Criar uma lista de alunos,.py
@@ -1,12 +1,12 @@
 #14. Criar uma lista de alunos, ir adicionando os nomes a essa lista e quando o usuário digitar “fim” ele encerra e mostra a lista de alunos.
-nome_A = 0
+nomeA = 0
 contador = 1
 lista = []
 while True:
-    nome_A = str(input(f"Qual o nome do {contador}º aluno ? 'Fim' para encerrar \n"))
-    if nome_A == "Fim":
+    nomeA = str(input(f"Qual o nome do {contador}º aluno ? 'Fim' para encerrar \n"))
+    if nomeA == "Fim":
         print ("Fim")
         break
-    lista.append (nome_A)
+    lista.append (nomeA)
     contador = contador + 1
 print (f"{lista}")
