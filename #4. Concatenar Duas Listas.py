78e49a63c482680854b96c5c37b465675bd679b8
SelinZ <91801180+SelinZ1@users.noreply.github.com>
2024-06-20 09:24:06 -0300

    Add files via upload

---
+++ #4. Concatenar Duas Listas.py
@@ -0,0 +1,10 @@
+#4. Concatenar Duas Listas:
+lista_1 = []
+lista_2 = []
+
+lista_1 = input("Digite os itens da lista 1 com espaço entre elas : ") .split()
+lista_2 = input("Digite os itens da lista 2 com espaços entre elas : ") .split()
+
+lista_3 = lista_1 + lista_2
+
+print (lista_3)
