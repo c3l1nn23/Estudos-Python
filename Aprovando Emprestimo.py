cf702c0452edfaddf44d175b68fc2763d3120c76
SelinZ <marcelo.dengo@gmail.com>
2024-12-19 18:12:27 -0300

    Create Aprovando Emprestimo.py

---
+++ Aprovando Emprestimo.py
@@ -0,0 +1,14 @@
+valor_casa = float(input("Digite o valor da casa: R$ "))
+salario = float(input("Digite o salário do comprador: R$ "))
+anos = int(input("Em quantos anos deseja pagar? "))
+
+meses = anos * 12
+prestacao_mensal = valor_casa / meses
+
+limite = salario * 0.30
+
+print(f"\nPara pagar uma casa de R${valor_casa:.2f} em {anos} anos, a prestação será de R${prestacao_mensal:.2f}.")
+if prestacao_mensal <= limite:
+    print("Empréstimo APROVADO!")
+else:
+    print("Empréstimo NEGADO! A prestação excede 30% do salário.")
